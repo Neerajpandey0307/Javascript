e3976252321608d9188466b6a0979a64a3dfb857
Neeraj Pandey <neeraj.pandey0307@gmail.com>
2025-08-14 15:08:57 +0000

    container changes done

---
+++ basics/variable.js
@@ -0,0 +1,9 @@
+const accountId = 154655 // const variable is constant if it declared it can't be changed after.
+let accountEmail = "neerajpandey.google.com" // let variable can be changed after declaring and its a better way to declare a variable if you need to change it .
+var accountPassword = "123458"// var variable is also like let but it's rearly used because it has some flause and issues.
+accountCity = "Delhi" // you can also declare a variable without writing the const , let or var . but its not recommended.
+let accountState; // in this variable be dosen't give the value. this is used when the values want to give later but you need the space.
+
+console.log(accountId) // this is used to print the variable values.
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]) // this is used to print variable values in tabular form.
