4d4487020aba4902b32568916531ed440463f412
Neeraj Pandey <neeraj.pandey0307@gmail.com>
2025-08-14 20:20:52 +0000

    datatypes changes done

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict";// treat all js code to new version
+// alert (3+3) this works on browser but there is different way to use alert in nodejs
+
+let name ="neeraj"// this is string data type
+let age =24 // this is integer datatype
+let haveYouDoneYourVaccination = true // this is boolean datatype
+let state; // this is undifined datatype
+
+// there are some types of datatype in js which we learn as we proceed in our learning. For example : symbol ,null and etc.
+
+//object
+console.log(typeof undefined);
+console.log(typeof(null))
