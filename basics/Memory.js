78bdc3c01016c73f94258abf69561686bd474b33
Neeraj Pandey <neerajpandey0307@gmail.com>
2025-11-03 19:29:15 +0530

    something

---
+++ basics/Memory.js
@@ -0,0 +1,15 @@
+// Memory
+
+// Stack(Primitive) and Heap Memory(Non Primitive or Reference)
+
+// @ STACK MEMORY - 
+
+
+let FirstelderWandOwner = "dumbledore" 
+let SecondelderWandOwner = FirstelderWandOwner
+SecondelderWandOwner = "harry"
+
+console.log(FirstelderWandOwner)
+console.log(SecondelderWandOwner)
+
+
--- basics/datatypes-summary.js
+++ basics/datatypes-summary.js
@@ -6,7 +6,7 @@
 @ JS engine automatically detects the type
 @ No strict type checking before runnig the code
 @ error related to type  come while running
-/*
+*/
 
 // DATATYPES ARE TWO TYPE
 
@@ -15,8 +15,34 @@
 
 // 7 TYPES -: String , Number , Boolean , Null , undefined, Symbol(to make any value unique), BigInt(some big value or scientific value)
 
- 
+let DatatypeString = "neeraj" 
+let DatatypeNumber  = 3
+let DatatypeBoolean = true
+let DatatypeNull = null // null means nothing not 0 not any other thing it means nothing 
+let DatatypeUndifined // undifined means the value is not given to this but a space had created.
+let DatatypeBigInt= 24143546765432n // it stores number but big numbers which is not possible to store in the NUMBER datatype.
+const id = symbol("2234");
+const anotherId =symbol("234")
+console.log(id===anotherId) // flase , because symbol treat every value as differnt , if they are same also , it use in production level where might some value clash , and they can be overwite.
+
 
 //  2) Reference or Non Primitive
 
-Array , Objects ,Functions
+// Array , Objects ,Functions
+ 
+
+
+const Marauders=["Sirius", "James", "Lupin", "peter" ];// array
+
+let animagous ={
+    name :"sirius black",
+    AnimalAtWhichAge: 15,
+    IsAlive : false
+
+} // object
+
+const MyFunction =function(){
+    console.log("hi")
+}
+
+// ************null typeof is object*********
